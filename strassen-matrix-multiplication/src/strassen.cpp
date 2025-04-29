d4bd39d68779e50cd61b720bca4ef2d20c020ce6
Salimalkharsa <salimalkharsa@gmail.com>
2025-04-29 01:22:05 -0500

    insert future and drake meme

--- strassen-matrix-multiplication/src/strassen.cpp
+++ strassen-matrix-multiplication/src/strassen.cpp
@@ -37,6 +37,7 @@ void multiply_strassen(int size, int **A, int **B, int **C)
 
     int half = size / 2;
 
+    // Create submatrix references
     int **A11 = new int *[half];
     int **A12 = new int *[half];
     int **A21 = new int *[half];
@@ -63,6 +64,7 @@ void multiply_strassen(int size, int **A, int **B, int **C)
     reference_submatrix(C21, C, half, 0, half);
     reference_submatrix(C22, C, half, half, half);
 
+    // Allocate result matrices
     int **M1 = allocate_matrix(half, half);
     int **M2 = allocate_matrix(half, half);
     int **M3 = allocate_matrix(half, half);
@@ -71,55 +73,67 @@ void multiply_strassen(int size, int **A, int **B, int **C)
     int **M6 = allocate_matrix(half, half);
     int **M7 = allocate_matrix(half, half);
 
-    int **T1 = allocate_matrix(half, half);
-    int **T2 = allocate_matrix(half, half);
+    // Allocate unique temp matrices for each task
+    int **T1_1 = allocate_matrix(half, half), **T2_1 = allocate_matrix(half, half);
+    int **T1_2 = allocate_matrix(half, half), **T2_2 = nullptr;
+    int **T1_3 = nullptr, **T2_3 = allocate_matrix(half, half);
+    int **T1_4 = nullptr, **T2_4 = allocate_matrix(half, half);
+    int **T1_5 = allocate_matrix(half, half), **T2_5 = nullptr;
+    int **T1_6 = allocate_matrix(half, half), **T2_6 = allocate_matrix(half, half);
+    int **T1_7 = allocate_matrix(half, half), **T2_7 = allocate_matrix(half, half);
+
+#pragma omp parallel
+#pragma omp single
+    {
+#pragma omp task
+        {
+            add_matrix(T1_1, A11, A22, half, half);
+            add_matrix(T2_1, B11, B22, half, half);
+            multiply_strassen(half, T1_1, T2_1, M1);
+        }
 
 #pragma omp task
-    {
-        add_matrix(T1, A11, A22, half, half);
-        add_matrix(T2, B11, B22, half, half);
-        multiply_strassen(half, T1, T2, M1);
-    }
+        {
+            add_matrix(T1_2, A21, A22, half, half);
+            multiply_strassen(half, T1_2, B11, M2);
+        }
 
 #pragma omp task
-    {
-        add_matrix(T1, A21, A22, half, half);
-        multiply_strassen(half, T1, B11, M2);
-    }
+        {
+            subtract_matrix(T2_3, B12, B22, half, half);
+            multiply_strassen(half, A11, T2_3, M3);
+        }
 
 #pragma omp task
-    {
-        subtract_matrix(T2, B12, B22, half, half);
-        multiply_strassen(half, A11, T2, M3);
-    }
+        {
+            subtract_matrix(T2_4, B21, B11, half, half);
+            multiply_strassen(half, A22, T2_4, M4);
+        }
 
 #pragma omp task
-    {
-        subtract_matrix(T2, B21, B11, half, half);
-        multiply_strassen(half, A22, T2, M4);
-    }
+        {
+            add_matrix(T1_5, A11, A12, half, half);
+            multiply_strassen(half, T1_5, B22, M5);
+        }
 
 #pragma omp task
-    {
-        add_matrix(T1, A11, A12, half, half);
-        multiply_strassen(half, T1, B22, M5);
-    }
+        {
+            subtract_matrix(T1_6, A21, A11, half, half);
+            add_matrix(T2_6, B11, B12, half, half);
+            multiply_strassen(half, T1_6, T2_6, M6);
+        }
 
 #pragma omp task
-    {
-        subtract_matrix(T1, A21, A11, half, half);
-        add_matrix(T2, B11, B12, half, half);
-        multiply_strassen(half, T1, T2, M6);
-    }
-
-#pragma omp task
-    {
-        subtract_matrix(T1, A12, A22, half, half);
-        add_matrix(T2, B21, B22, half, half);
-        multiply_strassen(half, T1, T2, M7);
+        {
+            subtract_matrix(T1_7, A12, A22, half, half);
+            add_matrix(T2_7, B21, B22, half, half);
+            multiply_strassen(half, T1_7, T2_7, M7);
+        }
     }
 
 #pragma omp taskwait
+
+    // Combine results into C
     for (int i = 0; i < half; ++i)
         for (int j = 0; j < half; ++j)
         {
@@ -129,6 +143,7 @@ void multiply_strassen(int size, int **A, int **B, int **C)
             C22[i][j] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
         }
 
+    // Free memory
     free_matrix(M1);
     free_matrix(M2);
     free_matrix(M3);
@@ -136,8 +151,17 @@ void multiply_strassen(int size, int **A, int **B, int **C)
     free_matrix(M5);
     free_matrix(M6);
     free_matrix(M7);
-    free_matrix(T1);
-    free_matrix(T2);
+
+    free_matrix(T1_1);
+    free_matrix(T2_1);
+    free_matrix(T1_2);
+    free_matrix(T2_3);
+    free_matrix(T2_4);
+    free_matrix(T1_5);
+    free_matrix(T1_6);
+    free_matrix(T2_6);
+    free_matrix(T1_7);
+    free_matrix(T2_7);
 
     delete[] A11;
     delete[] A12;
